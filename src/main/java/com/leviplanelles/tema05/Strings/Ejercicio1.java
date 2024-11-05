174375d5cc04465ec0f74cc3a788534170ba927c
Levi Planelles <leviplanellesgranata@gmail.com>
2024-11-05 17:32:15 +0100

    ej1 strings

--- src/main/java/com/leviplanelles/tema05/Strings/Ejercicio1.java
+++ src/main/java/com/leviplanelles/tema05/Strings/Ejercicio1.java
@@ -1,4 +1,19 @@
 package com.leviplanelles.tema05.Strings;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+
+    public static String primeraEnMayus(String cadena) {
+        char primerCaracter = cadena.toUpperCase().charAt(0);
+        return primerCaracter + cadena.substring(1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escribe una palabra con todo en minúsculas:");
+        String cadena = scanner.nextLine();
+        scanner.close();
+        System.out.println(primeraEnMayus(cadena));
+
+    }
 }
