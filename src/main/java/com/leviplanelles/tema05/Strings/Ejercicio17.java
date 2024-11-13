8b171243ba4481c4ca5532154c94978c8d15a70a
Levi Planelles <leviplanellesgranata@gmail.com>
2024-11-13 21:27:35 +0100

    18

--- src/main/java/com/leviplanelles/tema05/Strings/Ejercicio17.java
+++ src/main/java/com/leviplanelles/tema05/Strings/Ejercicio17.java
@@ -3,6 +3,11 @@ package com.leviplanelles.tema05.Strings;
 import com.leviplanelles.tema05.lib.IO;
 import static com.leviplanelles.tema05.Strings.Ejercicio16.calcularNIF;
 public class Ejercicio17 {
+    /**
+     * Este metodo te comprueba si el NIF introducido es correcto o no
+     * @param NIF un NIF
+     * @return devuelve true o false dependiendo de si es valido o no
+     */
     public static boolean comprobarNIF(String NIF) {
         String dni = NIF.substring(0, NIF.length()-1);
         int DNI = Integer.parseInt(dni);
--- src/main/java/com/leviplanelles/tema05/Strings/Ejercicio18.java
+++ src/main/java/com/leviplanelles/tema05/Strings/Ejercicio18.java
@@ -1,4 +1,41 @@
 package com.leviplanelles.tema05.Strings;
 
+import com.leviplanelles.tema05.lib.IO;
+
 public class Ejercicio18 {
+    /**
+     * Método para pedir una fecha y quitarle los espacios y los guiones
+     * @return devuelve la fecha pero solamente los numeros sin ningún espacio
+     */
+    public static String pedirFechaNac() {
+        String fecha = IO.solicitarString("Introduce tu fecha de nacimiento (dd-mm-aaaa): ",0,100);
+        String fechaSinEspacios = fecha.trim();
+        return fechaSinEspacios.replace("-","");
+    }
+
+    /**
+     * Método para calcular el número de la suerte
+     * @param fechaTodaJunta una fecha
+     * @return te devuelve el número de la suerte
+     */
+    public static int calcularLuckyNumber(String fechaTodaJunta) {
+        int total = 0;
+        int suma = 0;
+        char[] numeros = new char[fechaTodaJunta.length()];
+        for (int i = 0; i < fechaTodaJunta.length(); i++) {
+            numeros[i] = fechaTodaJunta.charAt(i);
+        }
+        for (int j = 0; j < numeros.length; j++) {
+            total += Character.getNumericValue(numeros[j]);
+        }
+        String res = String.valueOf(total);
+        char n1 = res.charAt(0);
+        char n2 = res.charAt(1);
+        return Character.getNumericValue(n1) + Character.getNumericValue(n2);
+    }
+    public static void main(String[] args) {
+        String fechaJunta = pedirFechaNac();
+        int numero = calcularLuckyNumber(fechaJunta);
+        System.out.println("Tu número de la suerte es el: " + numero);
+    }
 }
