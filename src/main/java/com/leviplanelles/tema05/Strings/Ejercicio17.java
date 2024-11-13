aef3d144080a72b459c99ff4bb4389687b15f6dd
Levi Planelles <leviplanellesgranata@gmail.com>
2024-11-13 16:12:01 +0100

    17

---
+++ src/main/java/com/leviplanelles/tema05/Strings/Ejercicio17.java
@@ -0,0 +1,18 @@
+package com.leviplanelles.tema05.Strings;
+
+import com.leviplanelles.tema05.lib.IO;
+import static com.leviplanelles.tema05.Strings.Ejercicio16.calcularNIF;
+public class Ejercicio17 {
+    public static boolean comprobarNIF(String NIF) {
+        String dni = NIF.substring(0, NIF.length()-1);
+        int DNI = Integer.parseInt(dni);
+        return NIF.equals(calcularNIF(DNI));
+    }
+
+    public static void main(String[] args) {
+        String NIF = IO.solicitarString("Ingresa tu NIF:",0,100);
+        boolean valido = comprobarNIF(NIF.trim().toUpperCase());
+        String res = valido ? "El NIF introducido es correcto" : "El NIF introducido es incorrecto!" ;
+        System.out.println(res);
+    }
+}
