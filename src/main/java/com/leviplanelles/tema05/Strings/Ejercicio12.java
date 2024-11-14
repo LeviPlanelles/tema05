a80bd3694456b1158135fd827b2d8161577a5fdf
Levi Planelles <leviplanellesgranata@gmail.com>
2024-11-14 18:47:17 +0100

    unos arreglos

--- src/main/java/com/leviplanelles/tema05/Strings/Ejercicio12.java
+++ src/main/java/com/leviplanelles/tema05/Strings/Ejercicio12.java
@@ -4,10 +4,9 @@ public class Ejercicio12 {
 
     public static String sustituirOcurrencias(String cadena) {
         return cadena.replaceAll("es","no por");
-
     }
     public static String sustituirDigitos(String cadena) {
-        return cadena.replaceAll("\\d","*");
+        return cadena.replaceAll("\\d+","*");
     }
 
     public static void main(String[] args) {
