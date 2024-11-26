0e3109bba290c17e14b1e707d95348f2777273b2
Levi Planelles <leviplanellesgranata@gmail.com>
2024-11-26 11:54:32 +0100

    busqueda linear de un array ordenado hecho

--- src/main/java/com/leviplanelles/tema05/Busqueda_Ordenacion/B_Lineal_Ordenado.java
+++ src/main/java/com/leviplanelles/tema05/Busqueda_Ordenacion/B_Lineal_Ordenado.java
@@ -1,7 +1,30 @@
 package com.leviplanelles.tema05.Busqueda_Ordenacion;
 
-public class B_Lineal_Ordenado {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class B_Lineal_Ordenado {
+
+    public static String buscarEnteroEnArrayOrdenado(int[] arr, int entero) {
+        String res = "";
+        int contador = 0;
+        if (arr == null) {
+            return "No hay ningún elemento en el array";
+        }else {
+            do {
+                if (arr[contador] == entero) {
+                    res = "Se ha encontrado el número " +entero+ " en la posición " + arr[contador] + " del array " + Arrays.toString(arr);
+                    break;
+                }else {
+                    res = "El número " + entero + " no está en el array " + Arrays.toString(arr);
+                }
+                contador++;
+            }while (arr[contador] <= entero);
+
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        int[] arrOrdenado = {12,22,23,43,55,86,99,111,203,465,488};
+        System.out.println(buscarEnteroEnArrayOrdenado(arrOrdenado,53));
     }
 }
